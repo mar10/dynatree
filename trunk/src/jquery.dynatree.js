e2d4183b0ef84cd4de196995958ae575e1754b43
moogle@wwWendt.de <moogle@wwWendt.de@31235841-f254-0410-8afa-8bf71294b5c9>
2010-05-04 11:17:15 +0000

    Dual licensed under the MIT or GPL Version 2 licenses (Issue 144)

--- trunk/src/jquery.dynatree.js
+++ trunk/src/jquery.dynatree.js
@@ -2,9 +2,10 @@
 	jquery.dynatree.js
 	Dynamic tree view control, with support for lazy loading of branches.
 
-	Copyright (c) 2008-2010  Martin Wendt (http://wwWendt.de)
-	Licensed under the MIT License (MIT-License.txt)
-
+	Copyright (c) 2008-2010,  Martin Wendt (http://wwWendt.de)
+	Dual licensed under the MIT or GPL Version 2 licenses.
+	http://code.google.com/p/dynatree/wiki/LicenseInfo
+	
 	A current version and some documentation is available at
 		http://dynatree.googlecode.com/
 
