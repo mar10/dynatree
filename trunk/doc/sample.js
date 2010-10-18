1cf40281b6f3bf7f0ade4af62a422ca02b30b199
moogle@wwwendt.de <moogle@wwwendt.de@31235841-f254-0410-8afa-8bf71294b5c9>
2010-10-18 17:42:12 +0000

    Fixed welcome page includes.

--- trunk/doc/sample.js
+++ trunk/doc/sample.js
@@ -5,15 +5,16 @@
 function viewSourceCode()
 {
 	window.location = "view-source:" + window.location.href;
-
 }
 
-function initCodeSamples() {
-	$('a.codeExample').each (
-			function( i ) {
-				$( this ).after( '<pre class="codeExample prettyprint"><code></code></pre>' );
-			}
-	)
+
+function initCodeSamples()
+{
+	$('a.codeExample').each(
+		function( i ) {
+			$( this ).after( '<pre class="codeExample prettyprint"><code></code></pre>' );
+		}
+	);
 	$( 'pre.codeExample' ).hide();
 	$('a.codeExample').toggle(
 			function() {
@@ -27,10 +28,10 @@ function initCodeSamples() {
 				$(this).html(this.old);
 				$(this.nextSibling).hide();
 			}
-	)
+	);
 	function parseCode(o){
 		if(!o.nextSibling.hascode){
-			$.get (o.href, function(code){
+			$.get(o.href, function(code){
 				// Doesn't work (only accepts simple/restricted html strings, not a full html page):
 //				logMsg("code.html: %o", $(code).html());
 
@@ -62,19 +63,17 @@ function initCodeSamples() {
 }
 
 
-
 $(function(){
 	// Log to Google Analytics, when not running locally
 	if ( document._gat && document.URL.toLowerCase().indexOf('wwwendt.de/')>=0 ) {
 		var pageTracker = _gat._getTracker("UA-316028-1");
 		pageTracker._trackPageview();
 	}
-
 	// Show some elements only, if (not) inside the Example Browser
-	if (top.location == self.location)
+	if (top.location == self.location){
 		$(".hideOutsideFS").hide();
-	else
+	}else{
 		$(".hideInsideFS").hide();
-
+	}
 	initCodeSamples();
 });
